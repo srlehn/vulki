17ed08c3b5ad322ef5428c5bf8a645bbd47de106
Simon Lehn <48837958+srlehn@users.noreply.github.com>
2026-02-25 01:20:54 +0100

    reduce shader api

--- shader/compiler.go
+++ shader/compiler.go
@@ -2,12 +2,10 @@ package shader
 
 import "github.com/gogpu/naga"
 
-// Compile compiles WGSL source code to SPIR-V binary using default options.
-func Compile(wgslSource string) ([]byte, error) {
-	return naga.Compile(wgslSource)
-}
-
-// CompileWithOptions compiles WGSL source code to SPIR-V with custom options.
-func CompileWithOptions(wgslSource string, opts naga.CompileOptions) ([]byte, error) {
-	return naga.CompileWithOptions(wgslSource, opts)
+// Compile compiles WGSL source code to SPIR-V with custom options.
+func Compile(wgslSource string, opts *naga.CompileOptions) ([]byte, error) {
+	if opts == nil {
+		opts = new(naga.DefaultOptions())
+	}
+	return naga.CompileWithOptions(wgslSource, *opts)
 }
